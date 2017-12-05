e4ce792164a05e4d35357ccff27fc9056bc358a4
Keith Mannock <keith@dcs.bbk.ac.uk>
2017-12-05 18:57:35 +0000

    revised blackjack

--- session-ten/blackjack.py
+++ session-ten/blackjack.py
@@ -1,5 +1,7 @@
 import random, os, sys
 
+# code courtesy of Stephanie Sunshine, mucked about abit by me.
+
 cardName = { 1: 'Ace', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', 11: 'Jack', 12: 'Queen', 13: 'King' }
 cardSuit = { 'c': 'Clubs', 'h': 'Hearts', 's': 'Spades', 'd': 'Diamonds' }
 
